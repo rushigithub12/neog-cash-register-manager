cf99631f81a8de3c416c1d9a4203f67bcc176580
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2022-05-13 18:20:25 +0530

    update: displaying after click

--- project-in-venillaJS/index.js
+++ project-in-venillaJS/index.js
@@ -3,45 +3,60 @@ const cashGiven = document.querySelector("#cashAmt");
 const btnTranslate = document.querySelector("#btnCheck");
 const message = document.querySelector("#msg");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const btnNext = document.querySelector("#btnNext");
+const outputBox = document.querySelector(".outputBox");
+const cashDiv = document.querySelector(".cash");
 
 let availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 btnTranslate.addEventListener("click", clickHandler);
+btnNext.addEventListener("click", nextClickHandler);
+
+function nextClickHandler(e) {
+  hideMessage();
+    if(billAmount.value > 0) {
+        cashDiv.style.display = "block";
+        btnNext.style.display = "none";
+    } else {
+        showMessage("enter valid amount");
+    }
+    e.preventDefault();
+}
 
 function clickHandler(e) {
-    hideMessage();
-    e.preventDefault();
+  hideMessage();
+  e.preventDefault();
+  let billAmt = Number(billAmount.value);
+  let cashAmt = Number(cashGiven.value);
 
-    let billAmt = Number(billAmount.value);
-    let cashAmt = Number(cashGiven.value);
-    
-    if(billAmt > 0 && cashAmt > 0) {
-        if( cashAmt >= billAmt ) {
-            const amountToReturn = cashAmt - billAmt;
-            calculateAmount(amountToReturn);
-        } else {
-            showMessage("Do you wanna wash the plates?");
-        }
-    } 
-    else {
-        showMessage("Enter valid amount");
+  outputBox.style.display = "block";
+
+  if (billAmt > 0 && cashAmt > 0) {
+    if (cashAmt >= billAmt) {
+      const amountToReturn = cashAmt - billAmt;
+      calculateAmount(amountToReturn);
+    } else {
+      showMessage("Do you wanna wash the plates?");
     }
+  } else {
+    showMessage("Enter valid amount");
+  }
 }
 
 function calculateAmount(amountToReturn) {
-    for(let i=0; i<availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc( amountToReturn / availableNotes[i] );
-        amountToReturn = amountToReturn % availableNotes[i];
-        noOfNotes[i].innerHTML = numberOfNotes;
-    }
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToReturn / availableNotes[i]);
+    amountToReturn = amountToReturn % availableNotes[i];
+    noOfNotes[i].innerHTML = numberOfNotes;
+  }
 }
 
 function hideMessage() {
-    message.style.display = "none";
+  message.style.display = "none";
 }
 
 function showMessage(msg) {
-    message.style.display = "block";
-    message.style.color = "red";
-    message.innerHTML = msg;
+  message.style.display = "block";
+  message.style.color = "red";
+  message.innerHTML = msg;
 }
